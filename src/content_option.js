dd086361eeb7b62e66d3900d1252ebfa482417b8
Muhammad Abubaker Siddiq <36122173+a4abubakar@users.noreply.github.com>
2023-09-07 23:58:29 +0500

    added src folder

---
+++ src/content_option.js
@@ -0,0 +1,203 @@
+const logotext = "";
+const meta = {
+    title: "Muhammad Abubaker Siddiq",
+    description: "I’m Muhammad Abubaker Siddiq Frontend Engineer, currently working in Pakistan",
+};
+
+const introdata = {
+    title: "I’m Muhammad Abubaker Siddiq",
+    animated: {
+        first: "I’m a frontend engineer",
+        second: "I code cool web apps",
+        third: "I develop mobile apps",
+    },
+    description: `Senior Web Developer specializing in front end development. Experienced with all stages of the 
+    development cycle for dynamic web projects. Strong background in project team lead and customer 
+    relations. Passionate front-end web developer with 6 years of experience using JavaScript, HTML5, CSS, 
+    React JS, Next JS and React Native to build all aspects of the user experience and user interface for client facing landing pages. Specializes in using React JS and Next JS to build cool web apps.`,
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Abit About My Self",
+    aboutme: `Senior Web Developer specializing in front end development. Experienced with all stages of the 
+    development cycle for dynamic web projects. Strong background in project team lead and customer 
+    relations. Passionate front-end web developer with 6 years of experience using JavaScript, HTML5, CSS, 
+    React JS, Next JS and React Native to build all aspects of the user experience and user interface for client facing landing pages. Specializes in using React JS and Next JS to build cool web apps.`,
+};
+const worktimeline = [{
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+},
+{
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+},
+];
+
+const skills = [{
+    name: "HTML",
+    value: 95,
+},
+{
+    name: "CSS",
+    value: 90,
+},
+{
+    name: "JavaScript",
+    value: 90,
+},
+{
+    name: "React",
+    value: 85,
+},
+{
+    name: "Firebase",
+    value: 85,
+},
+{
+    name: "Context API",
+    value: 80,
+},
+{
+    name: "Redux",
+    value: 75,
+},
+{
+    name: "React Native",
+    value: 75,
+},
+{
+    name: "TypeScript",
+    value: 70,
+},
+{
+    name: "Next JS",
+    value: 70,
+},
+];
+
+const services = [{
+    title: "Frontend Engineer – Pixel Edge, US (Feb 2022 to Present)",
+    description: <>
+        <li>Cooperate with designers to create clean interfaces and simple, intuitive interactions and experiences.</li>
+        <li>Develop project concepts and maintain optimal workflow</li>
+        <li>Work with senior developer to manage large, complex design projects for corporate clients.</li>
+        <li>Worked on React JS, Tailwind CSS and Bootstrap.</li>
+    </>
+},
+{
+    title: "Frontend Engineer – Expertizo, Karachi (Jan 2021 to Jan 2022)",
+    description: <>
+        <li>Complete detailed programming and development tasks for front end public and internal
+            websites as well as challenging back-end server code.
+        </li>
+        <li>Carry out quality assurance tests to discover errors and optimize usability.
+        </li>
+        <li>Worked on React JS, Next JS, React Native, Tailwind CSS, Bootstrap, Material UI, Unit Testing</li>
+    </>
+},
+{
+    title: "Full Stack Engineer – Fine Solutions, Karachi (Nov 2019 to Dec 2021)",
+    description: <>
+        <li>Worked in an agile environment with weekly stand ups, kept track of user stories/bugs in
+            GitHub projects and conducted number of hours of sprint planning and sprint retrospectives in
+            week
+        </li>
+        <li>
+            Pair programmed with multiple engineers and reviewed tram’s code to provide additional
+            perspective and catch previously missed errors.
+        </li>
+        <li>
+            Tested React components using Jest.
+        </li>
+        <li>
+            Worked on React JS, React Native, Bootstrap, Material UI and Ant Design.
+        </li>
+    </>
+},
+{
+    title: "Web & Mobile App Developer – Saylani Welfare & International Trust, Karachi (Jan 2017 to Oct 2019)",
+    description: <>
+        <li>Created and maintained front-end code for multiple ecommerce websites.
+        </li>
+        <li>Produced and assisted in the deployment of multiple marketing initiatives, including multiple
+            advertisements.
+        </li>
+        <li>Identified and implemented dynamic web solutions in accordance to company standards and
+            best practices.</li>
+        <li>Worked on React JS, React Native, Bootstrap, Material UI and Ant Design.</li>
+    </>
+},
+];
+
+const dataportfolio = [{
+    img: "https://www.my2cents.io/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimages%2FscholarshipBanner.c73cab66a96978cbc49248da58f7f887.png&w=384&q=100",
+    description: "This project is based on social network site of block chain. User can credit BSV to different users on the portal.",
+    link: "https://www.my2cents.io/",
+},
+{
+    img: "https://deenconnect.io/static/media/home-banner.e1f1d50a.png",
+    description: "This is a marketing website.",
+    link: "https://www.pixeledge.io/",
+},
+{
+    img: "https://mlezahcjg2uo.i.optimole.com/cb:mOcw.149bc/w:auto/h:auto/q:mauto/f:avif/https://www.uraudits.com/wp-content/uploads/2023/01/main-mobile_04.webp",
+    description: "This project is basically based on audits/inspection of multiple templates.",
+    link: "https://uraudits.com/",
+},
+{
+    img: "https://static.wixstatic.com/media/bf5ff7_3f321e28c12c44a3bfb780d51748fc21~mv2.png/v1/fill/w_950,h_664,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/bf5ff7_3f321e28c12c44a3bfb780d51748fc21~mv2.png",
+    description: "This project is basically based on currency management through these we can convert currency into other currency, can manage transaction as well as cash management.",
+    link: "https://www.treasurycube.com/",
+},
+{
+    img: "https://deenconnect.io/static/media/about-us-illustration.8dfaefe2.png",
+    description: "This project is basically based on connecting global muslims with local communities with different portals.",
+    link: "https://deenconnect.io/",
+    margin: "95px"
+},
+{
+    img: "https://i.imgur.com/vJowpL1.png",
+    description: "This project is basically based on providing online education to student with different courses.",
+    link: "https://outlier.org/",
+    margin: "95px"
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "abubakarsadiq6727@gmail.com",
+    YOUR_FONE: "(+92)343-3358774",
+    description: "Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you. Simply contact me using above details.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.comhttps://github.com/a4abubakar",
+    facebook: "https://facebook.com/MuhammadAubakerSiddiq",
+    linkedin: "https://linkedin.com/in/muhammad-abubaker-siddiq-27b37615b/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
